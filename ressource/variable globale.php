ce9c6161d10c5121312b898436c0ee1d64155011
Sarah-Boumier <110644514+Sarah-Boumier@users.noreply.github.com>
2022-08-06 23:52:07 +0200

    Update variable globale.php

--- ressource/variable globale.php
+++ ressource/variable globale.php
@@ -1,4 +1,4 @@
 <?php
-	$personal_id =  871066677819183104;
-	$link_pdp = "https://pbs.twimg.com/profile_images/656605984522158080/LZuOAY-o_normal.jpg";
-	$me  = "Alexandre (@Alexandre3004)";
+	$personal_id =  ;
+	$link_pdp = "";
+	$me  = "";
