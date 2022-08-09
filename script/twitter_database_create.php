305b848ccfe35c4eb5d2d07e0b0f689f9c7faccf
Sarah-Boumier <110644514+Sarah-Boumier@users.noreply.github.com>
2022-08-09 03:20:34 +0200

    Update twitter_database_create.php

--- script/twitter_database_create.php
+++ script/twitter_database_create.php
@@ -1,9 +1,9 @@
 <?php
 $conn = new mysqli('localhost', 'root', '');
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$req1 = mysqli_query($conn, "DROP DATABASE twitter2");
-$req2 = mysqli_query($conn, "CREATE DATABASE twitter2 DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci;");
-$db_select =  mysqli_select_db($conn, 'twitter2');
+$req1 = mysqli_query($conn, "DROP DATABASE twitter");
+$req2 = mysqli_query($conn, "CREATE DATABASE twitter DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci;");
+$db_select =  mysqli_select_db($conn, 'twitter');
 $req3 = mysqli_query($conn, "CREATE table datadm2 
 (
     id bigint, 
