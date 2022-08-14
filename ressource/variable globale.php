8bbb46a35c6cc02fb289ffd846c37ea6b72a0e43
Sarah-Boumier <110644514+Sarah-Boumier@users.noreply.github.com>
2022-08-14 12:05:39 +0200

    Update variable globale.php

--- ressource/variable globale.php
+++ ressource/variable globale.php
@@ -2,5 +2,5 @@
 	$personal_id =  ;
 	$link_pdp = "";
 	$me  = "";
-	$localhost = "localhost";
-	$root = "root"; 
+	$conn = new mysqli('localhost', 'root', '');
+	$db_select =  mysqli_select_db($conn, 'twitter');
