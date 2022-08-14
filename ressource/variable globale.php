7864f5b37b82f387a5e84b29f0739441cb35668f
Sarah-Boumier <110644514+Sarah-Boumier@users.noreply.github.com>
2022-08-15 01:44:58 +0200

    Update variable globale.php

--- ressource/variable globale.php
+++ ressource/variable globale.php
@@ -1,6 +1,7 @@
 <?php
-	$personal_id =  871066677819183104;
-	$link_pdp = "http://pbs.twimg.com/profile_images/1461386867413237762/gqtsVer1_normal.jpg";
-	$me  = "Sarah ❤ Eloïse 🇫🇷(@saraaah_lapute)";
+	$personal_id =  ;
+	$link_pdp = "";
+	$me  = "";
 	$conn = new mysqli('localhost', 'root', '');
 	$db_select =  mysqli_select_db($conn, 'twitter2');
+?>
