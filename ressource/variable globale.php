4456fedcf5d2ffbc21e4f7a848954e46d08f9320
Sarah-Boumier <110644514+Sarah-Boumier@users.noreply.github.com>
2022-08-15 01:41:21 +0200

    Add files via upload

---
+++ ressource/variable globale.php
@@ -0,0 +1,6 @@
+<?php
+	$personal_id =  871066677819183104;
+	$link_pdp = "http://pbs.twimg.com/profile_images/1461386867413237762/gqtsVer1_normal.jpg";
+	$me  = "Sarah ❤ Eloïse 🇫🇷(@saraaah_lapute)";
+	$conn = new mysqli('localhost', 'root', '');
+	$db_select =  mysqli_select_db($conn, 'twitter2');
