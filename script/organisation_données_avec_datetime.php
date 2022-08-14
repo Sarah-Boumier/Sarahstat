50595adc17a000fd237c0136ba6a5e51ed4fe542
Sarah-Boumier <110644514+Sarah-Boumier@users.noreply.github.com>
2022-08-14 21:32:37 +0200

    Update organisation_données_avec_datetime.php

--- script/organisation_données_avec_datetime.php
+++ script/organisation_données_avec_datetime.php
@@ -1,83 +1,89 @@
 <?php
-include '../ressource/variable globale.php';	
+include '../ressource/variable globale.php';
+include '../ressource/fonctions_utiles.php';
 mysqli_query($conn, "DELETE FROM datadm2");
-$url  = 'C:\wamp64\www\Twitter\ressource\direct-messages.js';
-$url2 = 'C:\wamp64\www\Twitter\ressource\direct-messages-part1.js';
-$page = file_get_contents($url);
 $posid1 = 0;
 $posid2 = 0;
 $posdatetime = 0;
 $i = 0;
-
-while ($posid1 = strpos($page, "recipientId", $posid1))
+ini_set('memory_limit', '-1');
+ while ($file = readdir($dir))
 {
-	$valueid1 = 0;
-	$valueid2 = 0;
-	$valueid3 = 0;
-	$posid1 = $posid1 + 16;
-	$postext = strpos($page, "text", $posid1) + 9;
-	$media = strpos($page, "mediaUrls", $postext) + 14;
-	$posid2 = strpos($page, "senderId", $postext) +13;
-	$id = strpos($page, "id", $posid2) +7;
-	$posdatetime = strpos($page, "createdAt", $posid2) + 14;
-	$datetime = "";
-	$mediaurl ="";
-	$dt = 0;
-	$a = 0;
-	$textcontent = "";
-	while($page[$posid1] != '"')
+	if (valid_name_file($file, "direct-messages.js") == 0 || valid_name_file($file, "direct-messages-part") == 0)
 	{
-		if (!$valueid1 =  ($page[$posid1]) + $valueid1 * 10)
-			echo "$i";
-		$posid1++;
+		$file = "../ressource/" . $file;
+		$page = file_get_contents($file);
+		while ($posid1 = strpos($page, "recipientId", $posid1))
+		{
+			$valueid1 = 0;
+			$valueid2 = 0;
+			$valueid3 = 0;
+			$posid1 = $posid1 + 16;
+			$postext = strpos($page, "text", $posid1) + 9;
+			$media = strpos($page, "mediaUrls", $postext) + 14;
+			$posid2 = strpos($page, "senderId", $postext) +13;
+			$id = strpos($page, "id", $posid2) +7;
+			$posdatetime = strpos($page, "createdAt", $posid2) + 14;
+			$datetime = "";
+			$mediaurl ="";
+			$dt = 0;
+			$a = 0;
+			$textcontent = "";
+			while($page[$posid1] != '"')
+			{
+				if (!$valueid1 =  ($page[$posid1]) + $valueid1 * 10)
+					echo "$i";
+				$posid1++;
+			}
+			while($page[$postext] != '"' || $page[$postext + 1] != ',')
+			{
+					$textcontent[$a] = $page[$postext];
+					$a++;
+					$postext++;
+			}
+			$a = 0;
+			while ($page[$media] != ']')
+			{
+				$mediaurl[$a] = $page[$media];
+				$a++;
+				$media++;
+			}
+			while($page[$posid2] != '"')
+			{
+				$valueid2 =  ($page[$posid2]) + $valueid2 * 10;
+				$posid2++;
+			}
+			while($page[$id] != '"')
+			{
+				$valueid3 =  ($page[$id]) + $valueid3 * 10;
+				$id++;
+			}
+			while($page[$posdatetime] != '"')
+			{
+				$datetime[$dt] = $page[$posdatetime];
+				$dt++;
+				$posdatetime++;
+			}
+			if ($valueid1 == $personal_id)
+				$er = 0;
+			else
+				$er = 1;
+			$schedule_date = new DateTime($datetime, new DateTimeZone('UTC'));
+			$schedule_date->setTimeZone(new DateTimeZone('Europe/Paris'));
+			$datetime =  $schedule_date->format('Y-m-d H:i:s');
+			$minuteoftheday  = (strtotime($datetime) / 60) % 1440;
+			$datetime = date('Y-m-d H:i:s', strtotime($datetime));
+				$textcontent =  mysqli_real_escape_string($conn, $textcontent);
+			mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+			$req =  mysqli_query($conn, "INSERT INTO datadm2  VALUES ($valueid1, $valueid2, '$datetime', $er, '$textcontent', '$mediaurl', $valueid3, $minuteoftheday)");
+			if ($req == false)
+			{
+				print_r("il y a un problème avec la requete");
+				die();
+			}
+			$posid1++;
+			$posdatetime++;
+			$i++;
+		}
 	}
-	while($page[$postext] != '"' || $page[$postext + 1] != ',')
-	{
-			$textcontent[$a] = $page[$postext];
-			$a++;
-			$postext++;
-	}
-	$a = 0;
-	while ($page[$media] != ']')
-	{
-		$mediaurl[$a] = $page[$media];
-		$a++;
-		$media++;
-	}
-	while($page[$posid2] != '"')
-	{
-		$valueid2 =  ($page[$posid2]) + $valueid2 * 10;
-		$posid2++;
-	}
-	while($page[$id] != '"')
-	{
-		$valueid3 =  ($page[$id]) + $valueid3 * 10;
-		$id++;
-	}
-	while($page[$posdatetime] != '"')
-	{
-		$datetime[$dt] = $page[$posdatetime];
-		$dt++;
-		$posdatetime++;
-	}
-	if ($valueid1 == $personal_id)
-		$er = 0;
-	else
-		$er = 1;
-	$schedule_date = new DateTime($datetime, new DateTimeZone('UTC'));
-	$schedule_date->setTimeZone(new DateTimeZone('Europe/Paris'));
-	$datetime =  $schedule_date->format('Y-m-d H:i:s');
-	$minuteoftheday  = (strtotime($datetime) / 60) % 1440;
-	$datetime = date('Y-m-d H:i:s', strtotime($datetime));
-		$textcontent =  mysqli_real_escape_string($conn, $textcontent);
-	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-	$req =  mysqli_query($conn, "INSERT INTO datadm2  VALUES ($valueid1, $valueid2, '$datetime', $er, '$textcontent', '$mediaurl', $valueid3, $minuteoftheday)");
-	if ($req == false)
-	{
-		print_r("il y a un problème avec la requete");
-		die();
-	}
-	$posid1++;
-	$posdatetime++;
-	$i++;
-}
+ }
