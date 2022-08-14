ac2ce8fe4c5eb982374ce4be04bde8a24582b16f
Sarah-Boumier <110644514+Sarah-Boumier@users.noreply.github.com>
2022-08-14 21:51:05 +0200

    Update organisation_données_avec_datetime.php

--- script/organisation_données_avec_datetime.php
+++ script/organisation_données_avec_datetime.php
@@ -1,6 +1,22 @@
 <?php
 include '../ressource/variable globale.php';
 include '../ressource/fonctions_utiles.php';
+$conn = new mysqli('localhost', 'root', '');
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$req1 = mysqli_query($conn, "DROP DATABASE twitter");
+$req2 = mysqli_query($conn, "CREATE DATABASE twitter DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci;");
+$db_select =  mysqli_select_db($conn, 'twitter');
+$req3 = mysqli_query($conn, "CREATE table datadm2 
+(
+    id bigint, 
+	id2 bigint,
+	datetimes datetime,
+	er int,
+	message text,
+	media text,
+	id3 text,
+	minute int
+)");
 mysqli_query($conn, "DELETE FROM datadm2");
 $posid1 = 0;
 $posid2 = 0;
