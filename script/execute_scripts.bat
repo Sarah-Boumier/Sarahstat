8e302a3de772a134015f55e3422159a61f51cb5e
Sarah-Boumier <110644514+Sarah-Boumier@users.noreply.github.com>
2022-08-19 15:05:43 +0200

    Update execute_scripts.bat

--- script/execute_scripts.bat
+++ script/execute_scripts.bat
@@ -1,5 +1,5 @@
 php organisation_donnees_avec_datetime.php
 @ECHO  BASE DE DONNEES REMPLIE AVEC SUCCES
 php id-to-name.php
-@ECHO  nom et handle de vos contacts recuperes
+@ECHO  NOM ET HANDLE DE VOS CONTACTS RECUPERES
 PAUSE
