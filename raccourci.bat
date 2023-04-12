215625c11728122c21d8b41da61e559701a20746
Sarah-Boumier <110644514+Sarah-Boumier@users.noreply.github.com>
2023-04-12 14:19:12 +0200

    Update raccourci.bat

--- raccourci.bat
+++ raccourci.bat
@@ -3,8 +3,8 @@ set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
 echo sLinkFile = "%USERPROFILE%\Desktop\Sarahstat.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
-echo oLink.TargetPath = "C:\wamp64\www\Twitter\Classement-de-vos-dm-twitter-main\launch sarahstat.bat" >> %SCRIPT%
-echo oLink.IconLocation = "C:\wamp64\www\Twitter\Classement-de-vos-dm-twitter-main\image\db.ico">> %SCRIPT%
+echo oLink.TargetPath = "C:\wamp64\www\Twitter\launch sarahstat.bat" >> %SCRIPT%
+echo oLink.IconLocation = "C:\wamp64\www\Twitter\image\db.ico">> %SCRIPT%
 echo oLink.Arguments = "-h Window7 -a ifix" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
 cscript /nologo %SCRIPT%
